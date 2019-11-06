67ec2f2d64910f27db1f0141cda8eb9c9cc93ae4
Nathaniel Stack <nathanielstack@Nathaniels-MacBook-Pro.local>
2019-11-06 11:53:55 -0800

    added basic views for all blogs and individual ones

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import BlogPost
+
 
 # Create your views here.
+def blog_list(request):
+    blogs = BlogPost.objects.all()
+    return render(request, 'blog/blog_list.html', {'blogs' : blogs})
+
+def blog_detail(request, pk):
+    blog = BlogPost.objects.get(id=pk)
+    return render(request, 'blog/blog_detail.html', {'blog' : blog})
+
+def blog_category(request, category):
+    blogs = BlogPost.objects.filter(tags__contains=[category])
+    return render(request, 'blog/blog_list.html', {'blogs' : blogs})
+    
